5ac9516b417de5f9edd4d33efad6c515407c6401
Kevin Pei <kevin-pei@hotmail.com>
2015-11-28 18:10:39 -0500

    captions

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ $(function(){
       var halfHeight = $(this).outerHeight()/2*maxScale;
       var top = centerY - halfHeight;
       var left = centerX - halfWidth;
-      $(this).css('transform', 'scale('+maxScale+') translate('+(left-15)*-1+'px,'+(top-15+$('#navbar').outerHeight())*-1+'px)');
+      $(this).css('transform', 'scale('+maxScale+') translate('+(15-left)*-1+'px,'+(15-top+$('#navbar').outerHeight())*-1+'px)');
     }else{
       $(this).css('transform', '');
     }

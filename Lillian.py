e343ee5b0d9a2b66146ae490be614066f2a04a48
LillianZ <lillianzhang1998@gmail.com>
2015-11-28 12:44:15 -0500

    its a function now!
    YaY

--- Lillian.py
+++ Lillian.py
@@ -39,20 +39,25 @@ def get_image_size(fname):
         else:
             return
         return width, height
-URL = sys.argv[1]
-default_dir = "LillianImages"
-opener = urllib2.build_opener()
-urllib2.install_opener(opener)
-soup = BeautifulSoup(urllib2.urlopen(URL).read())
-imgs = soup.findAll("img",{"alt":True, "src":True})
-for img in imgs:
-    img_url = img["src"]
-    if img_url[:4] == "http":
-        filename = os.path.join(default_dir, img_url.split("/")[-1])
-        img_data = opener.open(img_url)
-        f = open(filename,"wb")
-        f.write(img_data.read())
-        f.close()
-        size = get_image_size(filename)
-        if size[0] < 200 or size[1] < 100:
-            os.remove(filename)
+
+def get_images(URL):
+    default_dir = "LillianImages"
+    opener = urllib2.build_opener()
+    urllib2.install_opener(opener)
+    soup = BeautifulSoup(urllib2.urlopen(URL).read())
+    imgs = soup.findAll("img",{"alt":True, "src":True})
+
+    for img in imgs:
+        img_url = img["src"]
+        alt = img.get("alt","")
+        print alt
+        if img_url[:4] == "http":
+            filename = os.path.join(default_dir, img_url.split("/")[-1])
+            img_data = opener.open(img_url)
+            f = open(filename,"wb")
+            f.write(img_data.read())
+            f.close()
+            size = get_image_size(filename)
+            if size[0] < 200 or size[1] < 150:
+                os.remove(filename)
+    return

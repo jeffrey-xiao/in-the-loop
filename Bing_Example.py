5efcace767a5d0ef5e40771e24718c3970849637
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-11-28 02:55:19 -0500

    added backend stuff

--- Bing_Example.py
+++ Bing_Example.py
@@ -1,15 +1,90 @@
 import urllib2
 import json
+import Queue
 from twitter import *
+
+filein = open("words.txt", "r")
+
+class Node:
+    def __init__ (self, depth, index):
+        self.depth = depth
+        self.index = index
+        self.child = [None]*128
+        self.c = []
+        self.isEnd = False
+        self.fall = None
+        self.parent = None
+    def addWord (self, s):
+        if self.depth == len(s):
+            self.isEnd = True
+            return
+        curr = s[self.depth]
+        index = ord(curr)
+        if self.child[index] == None:
+            self.child[index] = Node(self.depth + 1, index)
+            self.child[index].parent = self
+            self.c += [index]
+        self.child[index].addWord(s)
+
+def computeFall ():
+    q = Queue.Queue()
+    root.fall = root
+    q.put(root)
+    while not q.empty():
+        curr = q.get()
+        for i in curr.c:
+            q.put(curr.child[i])
+        if curr.fall != None:
+            continue
+        fall = curr.parent.fall
+        while fall.child[curr.index] == None and fall != root:
+            fall = fall.fall
+        curr.fall = fall.child[curr.index]
+        if curr.fall == None or curr.fall == curr:
+            curr.fall = root
+            
+def printWord (n):
+    if n != root:
+        return printWord(n.parent) + chr(n.index)
+    return ""
+
+def searchDictionary (s):
+    ret = []
+    currState = root
+    for i in range(len(s)):
+        curr = s[i]
+        index = ord(curr)
+        while currState.child[index] == None and currState != root:
+            currState = currState.fall
+        if currState == root:
+            if currState.child[index] != None:
+                currState = currState.child[index]
+        else:
+            currState = currState.child[index]
+        
+        other = currState
+        while other != root:
+            if other.isEnd:
+                ret += [str(printWord(other))]
+            other = other.fall
+    return ret
+
+root = Node(0, 0)
+root.parent = root;
+
+def init ():
+    for word in filein:
+        root.addWord(word.lower().strip())
+    computeFall()
 
 keyBing = 'w9Wv9QcXG2TrFgSdVdXNlcdDioOzGBmlNFhlu4994qk'
 credentialBing = 'Basic ' + (':%s' % keyBing).encode('base64')[:-1] # the "-1" is to remove the trailing "\n" which encode adds
 top = 10
 offset = 0
 
-def search (keyword):
+def searchKeyword (keyword):
     url = 'https://api.datamarket.azure.com/Bing/Search/News?' + \
-      'Query=%s&$top=%d&$skip=%d&$format=json' % (keyword, top, offset)
+      'Query=%s&$top=%d&$skip=%d&$format=json' % ("%27"+keyword+"%27", top, offset)
     request = urllib2.Request(url)
     request.add_header('Authorization', credentialBing)
     requestOpener = urllib2.build_opener()
@@ -22,8 +97,17 @@ execfile("config.py", config)
 
 twitter = Twitter(auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
 
-results = twitter.trends.place(_id = 1)
+results = twitter.trends.place(_id = 23424775)
 
 for location in results:
     for trend in location["trends"]:
-        print " - %s" % trend["name"]
+        keyword = trend["name"]
+        if keyword != None:
+            if keyword[0] == '#':
+                keyword = keyword[1:]
+            # process keyword
+            print "Results for " + keyword
+            results = searchKeyword(keyword.replace(" ","+"))
+            for x in range(len(results)):
+                print results[x]["Url"]
+            
--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #    https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "XxXxXxxXXXxxxxXXXxXX"
-consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
-access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
-access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
+consumer_key = "TWoaxwf0SFfS6234wDBRVB2qx"
+consumer_secret = "RIMbhk2DeWK7V3bbUvLrkCtJUzJ2AaQOL1b924Qf1crH0wJ2X8"
+access_key = "1679108629-Sjap3PsRx1MFpki32xeT2Z3RZ6fjw7Bk1IwPmm3"
+access_secret = "eeUVjbuqU94AUqry5rR7KBovb27TAUZYUWHpB9NKopiiw"
